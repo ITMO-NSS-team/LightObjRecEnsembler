13452638394953faa63486d23f0e12d7c6724f44
magleb <maximovglebigorevich@gmail.com>
2021-10-06 15:55:04 +0300

    experiment for 4 models

--- baseline_VHR/VHR_experiment.py
+++ baseline_VHR/VHR_experiment.py
@@ -56,14 +56,19 @@ def ensemble_OD_predictions(bboxes: list,
     labels_merged = []
     scores_merged = []
 
+    chosen_box = None
+    chosen_label = None
+    chosen_score = None
+    check_flag = True
+
     for index_1, first_box in enumerate(bboxes[weak_model_ind]):
         for index_2, second_box in enumerate(bboxes[best_model_ind]):
             ratio, intersect_coord = rectangle_intersect(first_box, second_box)
             if ratio is None:
                 check_flag = True
             elif sum(ratio) > area_threshold:
-                CW1 = val_weights[best_model_ind] * scores[weak_model_ind][index_1]
-                CW2 = val_weights[weak_model_ind] * scores[best_model_ind][index_2]
+                CW1 = weights[best_model_ind] * scores[weak_model_ind][index_1]
+                CW2 = weights[weak_model_ind] * scores[best_model_ind][index_2]
                 if labels[weak_model_ind][index_1] == labels[best_model_ind][index_2]:
                     chosen_box = second_box
                     chosen_label = labels[best_model_ind][index_2]
@@ -78,7 +83,7 @@ def ensemble_OD_predictions(bboxes: list,
                 check_flag = False
                 break
 
-        if check_flag:
+        if check_flag and chosen_box is not None:
             bboxes_merged.append(chosen_box)
             labels_merged.append(chosen_label)
             scores_merged.append(chosen_score)
@@ -167,6 +172,30 @@ model_3 = get_fasterRCNN_resnet(num_classes=params['CLASSES'],
                               aspect_ratios=params['ASPECT_RATIOS'],
                               min_size=params['MIN_SIZE'],
                               max_size=params['MAX_SIZE'])
+params = {'BATCH_SIZE': 32,
+          'LR': 0.001,
+          'PRECISION': 32,
+          'CLASSES': num_classes,
+          'SEED': 42,
+          'PROJECT': 'Heads',
+          'EXPERIMENT': 'heads',
+          'MAXEPOCHS': 500,
+          'BACKBONE': 'densenet121',
+          'ANCHOR_SIZE': ((32, 64, 128, 256, 512),),
+          'ASPECT_RATIOS': ((0.5, 1.0, 2.0),),
+          'MIN_SIZE': 1024,
+          'MAX_SIZE': 1024,
+          'IMG_MEAN': [0.485, 0.456, 0.406],
+          'IMG_STD': [0.229, 0.224, 0.225],
+          'IOU_THRESHOLD': 0.5
+          }
+
+model_4 = get_fasterRCNN_resnet(num_classes=params['CLASSES'],
+                              backbone_name=params['BACKBONE'],
+                              anchor_size=params['ANCHOR_SIZE'],
+                              aspect_ratios=params['ASPECT_RATIOS'],
+                              min_size=params['MIN_SIZE'],
+                              max_size=params['MAX_SIZE'])
 
 save = True
 show = False
@@ -175,19 +204,23 @@ device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cp
 filepath = "../local/VHR_statedict_fasterrcnn_resnet50_fpn.pth"
 filepath_2 = "../local/VHR_statedict_resnet18.pth"
 filepath_3 = "../local/VHR_statedict_mobilenet_v3_large.pth"
+filepath_4 = "../local/VHR_statedict_densenet121.pth"
 path = os.path.dirname(os.path.abspath(__file__))
-path_prediction = os.path.join(path, 'NWPU VHR-10 dataset', 'predictions_mobilev3_resnet50')
+path_prediction = os.path.join(path, 'NWPU VHR-10 dataset', 'predictions_4_models')
 
 if __name__ == '__main__':
     model.load_state_dict(torch.load(filepath))
     model_2.load_state_dict(torch.load(filepath_2))
     model_3.load_state_dict(torch.load(filepath_3, map_location=device))
+    model_4.load_state_dict(torch.load(filepath_4, map_location=device))
 
     model.eval()
     model_2.eval()
     model_3.eval()
+    model_4.eval()
 
-    val_weights = validation_weights([model, model_2, model_3], dataset_val)
+    # val_weights = validation_weights([model, model_2, model_3, model_4], dataset_val)
+    val_weights = [0.5279900411868104, 0.04605057596583206, 0.11095235215754355, 0.31500703068981406]
 
     columns = [
         "AP",
@@ -207,9 +240,11 @@ if __name__ == '__main__':
     results_resnet50 = pd.DataFrame(columns=columns)
     results_resnet18 = pd.DataFrame(columns=columns)
     results_mobilev3 = pd.DataFrame(columns=columns)
+    results_densenet121 = pd.DataFrame(columns=columns)
     results_resnet50_nms = pd.DataFrame(columns=columns)
     results_resnet18_nms = pd.DataFrame(columns=columns)
     results_mobilev3_nms = pd.DataFrame(columns=columns)
+    results_densenet121_nms = pd.DataFrame(columns=columns)
     results_ensemble_nms = pd.DataFrame(columns=columns)
 
     for i in range(len(dataset_test)):
@@ -224,30 +259,42 @@ if __name__ == '__main__':
             prediction = model([img.to(device)])[0]
             prediction_2 = model_2([img.to(device)])[0]
             prediction_3 = model_3([img.to(device)])[0]
+            prediction_4 = model_4([img.to(device)])[0]
 
         ### VISUALISE MODELS PREDICTIONS
         metrics_50 = visualise_model_prediction(prediction, target, img, image_id, save, show, path_prediction, 'RESNET50')
         metrics_18 = visualise_model_prediction(prediction_2, target, img, image_id, save, show, path_prediction, 'RESNET18')
         metrics_v3 = visualise_model_prediction(prediction_3, target, img, image_id, save, show, path_prediction, 'mobilenet_v3')
+        metrics_121 = visualise_model_prediction(prediction_4, target, img, image_id, save, show, path_prediction, 'densenet_121')
 
         ### VISUALISE MODELS PREDICTIONS AFTER IOU_THERESHOLD
         nms_prediction, metrics_50_nms = visualise_model_prediction_nms(prediction, target, img, image_id, save, show, path_prediction, 'RESNET50_NMS')
         nms_prediction_2, metrics_18_nms = visualise_model_prediction_nms(prediction_2, target, img, image_id, save, show, path_prediction, 'RESNET18_NMS')
         nms_prediction_3, metrics_v3_nms = visualise_model_prediction_nms(prediction_3, target, img, image_id, save, show, path_prediction, 'mobilenet_v3_NMS')
+        nms_prediction_4, metrics_121_nms = visualise_model_prediction_nms(prediction_4, target, img, image_id, save, show, path_prediction,'densenet_121_NMS')
 
-        all_prediction = [nms_prediction, nms_prediction_2, nms_prediction_3]
+        all_prediction = [nms_prediction, nms_prediction_2, nms_prediction_3, nms_prediction_4]
 
         ### VISUALISE MODELS PREDICTIONS AFTER ENSEMBLING
         all_prediction = filtering_ensemble(all_prediction, val_weights, image_id)
+        all_val_weights = val_weights.copy()
+
+        for i, pred in enumerate(all_prediction):
+            plot_img_bbox(img, pred, title=f'{i}_filter', save=save,
+                          image_id=image_id, show=show, path=path_prediction)
 
         while len(all_prediction) != 1:
+            inter_val_weights = []
             nms_pred = all_prediction.pop()
             nms_pred_2 = all_prediction.pop()
+            inter_val_weights.append(all_val_weights.pop())
+            inter_val_weights.append(all_val_weights.pop())
             compose_bbox = ensemble_OD_predictions([nms_pred['boxes'], nms_pred_2['boxes']],
                                                    [nms_pred['labels'], nms_pred_2['labels']],
                                                    [nms_pred['scores'], nms_pred_2['scores']],
-                                                   weights=val_weights)
+                                                   weights=inter_val_weights)
             all_prediction.append(compose_bbox)
+            all_val_weights.append(max(inter_val_weights))
         compose_bbox = all_prediction[0]
 
         plot_img_bbox(img, compose_bbox, title='ENSEMBLE', save=save,
@@ -255,11 +302,12 @@ if __name__ == '__main__':
         metrics_ensemble_nms = calculate_coco_metrics(target, compose_bbox)
 
         ### SAVE METRICS FOR CURRENT IMAGE
-        all_results = [results_resnet50, results_resnet18, results_mobilev3,
-                       results_resnet50_nms, results_resnet18_nms, results_mobilev3_nms, results_ensemble_nms]
-        all_metrics = [metrics_50, metrics_18, metrics_v3, metrics_50_nms,
-                       metrics_18_nms, metrics_v3_nms, metrics_ensemble_nms]
-        all_names = ['resnet50', 'resnet18', 'mobilenet_v3', 'resnet50_nms', 'resnet18_nms', 'mobilenet_v3_nms', 'ensemble']
+        all_results = [results_resnet50, results_resnet18, results_mobilev3, results_densenet121, results_resnet50_nms,
+                       results_resnet18_nms, results_mobilev3_nms, results_densenet121_nms, results_ensemble_nms]
+        all_metrics = [metrics_50, metrics_18, metrics_v3, metrics_121, metrics_50_nms,
+                       metrics_18_nms, metrics_v3_nms, metrics_121_nms, metrics_ensemble_nms]
+        all_names = ['resnet50', 'resnet18', 'mobilenet_v3', 'densenet_121', 'resnet50_nms',
+                     'resnet18_nms', 'mobilenet_v3_nms', 'densenet_121', 'ensemble']
 
         for res, met in zip(all_results, all_metrics):
             res = res.append(met, ignore_index=True)
--- baseline_VHR/utils/utils.py
+++ baseline_VHR/utils/utils.py
@@ -15,7 +15,7 @@ def visualise_model_prediction(prediction, target, img, image_id, save, show, pa
 
 
 def visualise_model_prediction_nms(prediction, target, img, image_id, save, show, path_prediction, title):
-    nms_prediction = apply_nms(prediction, iou_thresh=0.1)
+    nms_prediction = apply_nms(prediction, iou_thresh=0.05)
     nms_prediction['boxes'] = nms_prediction['boxes'].cpu().numpy()
     nms_prediction['labels'] = nms_prediction['labels'].cpu().numpy()
     nms_prediction['scores'] = nms_prediction['scores'].cpu().numpy()
